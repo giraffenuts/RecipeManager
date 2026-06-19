9118cb96836db133d41ce9b901bbc494ee789ae5
giraffenuts <cartergraefnitz@gmail.com>
2026-06-19 16:26:11 -0400

    RM: Added diagram directory and test file

--- Food.cpp
+++ Food.cpp
@@ -1,4 +1,4 @@
 #include <string>
 #include "Food.h"
-
+// Define non-trivial member functions for Food class
 
--- main.cpp
+++ main.cpp
@@ -3,8 +3,5 @@
 
 int main()
 {
-    Food apple { "apple", 2, 7, false };
-    apple.printName();
 
-    return 0;
 }
